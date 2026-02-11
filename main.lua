49a240aab5a1e795928be1d12dcaa1b11af28855
Hayden Frank <haydog2006@gmail.com>
2026-02-11 00:49:24 -0500

    feat: added whale background

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
 function love.draw()
+	love.graphics.draw(Whale, 300, 200)
 	love.graphics.print("Hello Earth!", 400, 300)
 end
+
+function love.load()
+	Whale = love.graphics.newImage("gfx/whale.jpeg")
+end
+
+-- function love.load()
+-- 	sound = love.audio.newSource("music.ogg", "stream")
+-- 	love.audio.play(sound)
+-- end
