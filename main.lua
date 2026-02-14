13dfbec999b6effdcc3246cb8121aa5d3b3373ea
PortGrammy <porterjgramling@outlook.com>
2026-02-13 22:07:39 -0500

    -gehhh im so nervous... idk what to call it... heh... blank cd thing or something

--- main.lua
+++ main.lua
@@ -15,26 +15,26 @@ function love.load()
 	static = {}
 
 	static.roof = {}
-	static.roof.b = love.physics.newBody(world, 0, 0, "static")
-	static.roof.s = love.physics.newRectangleShape(400, 1)
+	static.roof.b = love.physics.newBody(world, 200, 0, "static")
+	static.roof.s = love.physics.newRectangleShape(400, 5)
 	static.roof.f = love.physics.newFixture(static.roof.b, static.roof.s)
 	static.roof.f:setUserData("Roof")
 
 	static.wallr = {}
 	static.wallr.b = love.physics.newBody(world, 400, 200, "static")
-	static.wallr.s = love.physics.newRectangleShape(1, 400)
+	static.wallr.s = love.physics.newRectangleShape(5, 400)
 	static.wallr.f = love.physics.newFixture(static.wallr.b, static.wallr.s)
 	static.wallr.f:setUserData("WallR")
 
 	static.walll = {}
 	static.walll.b = love.physics.newBody(world, 0, 200, "static")
-	static.walll.s = love.physics.newRectangleShape(1, 400)
+	static.walll.s = love.physics.newRectangleShape(5, 400)
 	static.walll.f = love.physics.newFixture(static.walll.b, static.walll.s)
 	static.walll.f:setUserData("WallL")
 
 	static.ground = {}
 	static.ground.b = love.physics.newBody(world, 200, 400, "static")
-	static.ground.s = love.physics.newRectangleShape(400, 1)
+	static.ground.s = love.physics.newRectangleShape(400, 5)
 	static.ground.f = love.physics.newFixture(static.ground.b, static.ground.s)
 	static.ground.f:setUserData("Ground")
 end
@@ -45,23 +45,23 @@ end
 
 function love.draw()
 
-	-- love.graphics.setColor(1, 0, 0)
-    -- love.graphics.polygon("line",
-    --     static.ground.b:getWorldPoints(static.ground.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     static.wallr.b:getWorldPoints(static.wallr.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     static.walll.b:getWorldPoints(static.walll.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     static.roof.b:getWorldPoints(static.roof.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     box.b:getWorldPoints(box.s:getPoints())
-    -- )
-    -- love.graphics.setColor(1, 1, 1)
+	love.graphics.setColor(1, 0, 0)
+    love.graphics.polygon("line",
+        static.ground.b:getWorldPoints(static.ground.s:getPoints())
+    )
+    love.graphics.polygon("line",
+        static.wallr.b:getWorldPoints(static.wallr.s:getPoints())
+    )
+    love.graphics.polygon("line",
+        static.walll.b:getWorldPoints(static.walll.s:getPoints())
+    )
+    love.graphics.polygon("line",
+        static.roof.b:getWorldPoints(static.roof.s:getPoints())
+    )
+    love.graphics.polygon("line",
+        box.b:getWorldPoints(box.s:getPoints())
+    )
+    love.graphics.setColor(1, 1, 1)
 
 	love.graphics.rectangle("fill", box.b:getX() - 30, box.b:getY() - 30, 60, 60)
 end
